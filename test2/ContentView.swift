2067b279b77a1bdb50ea3fb0097014d7d2b785af
Sumanth Kalluru <kallurusumanth@gmail.com>
2024-02-21 01:18:25 -0600

    Initial Commit

---
+++ test2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test2
+//
+//  Created by Sumanth Kalluru on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Sumanth!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
