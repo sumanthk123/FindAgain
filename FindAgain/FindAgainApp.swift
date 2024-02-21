767c47b40e80d0e8b21fcb631ba836a54077af3b
Chidera Ibe <chideraibe482005@gmail.com>
2024-02-21 01:37:53 -0600

    Initial Commit

---
+++ FindAgain/FindAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindAgainApp.swift
+//  FindAgain
+//
+//  Created by Chidera on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct FindAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
