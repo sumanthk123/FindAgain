5624a8fdc494d601e0701dca83cdcd1039b06312
Chidera Ibe <chideraibe482005@gmail.com>
2024-02-22 16:47:05 -0600

    Some random changes to the found page

--- FindAgain/ContentView.swift
+++ FindAgain/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var search = listOfItems
+    @State var searchText = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Image("icons8-filter-50")
+                Text("Found")
+                Image("icons8-location-50")
+            }.frame(maxWidth: .infinity).background(Color.indigo)
+            Spacer()
         }
-        .padding()
+        VStack {
+
+            NavigationView {
+                List {
+                    
+                }
+                .searchable(text: $searchText)
+                .background(Color.indigo)
+            }
+        }
+        
+        
+    }
+    var things: [String] {
+        let lcItems = search.map {$0.lowercased() }
+        return  searchText == "" ? lcItems : lcItems.filter {
+            $0.contains(searchText.lowercased())
+        }
     }
 }
 
---
+++ FindAgain/SampleData.swift
@@ -0,0 +1,14 @@
+//
+//  SampleData.swift
+//  FindAgain
+//
+//  Created by Chidera on 2/22/24.
+//
+
+import Foundation
+
+public var listOfItems = [
+    "Airpods",
+    "ID"
+]
+
